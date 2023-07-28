6671ec140517e9d9de7d16b05e9d8f31b179ac72
kate mwende <kmwende419@gmail.com>
2023-07-28 20:47:09 +0300

    Settle all promises and return an array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,14 @@
-// eslint-disabled-next-2-lines
+// eslint-disable-next-line
 import signUpUser from './4-user-promise.js';
+// eslint-disable-next-line
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
