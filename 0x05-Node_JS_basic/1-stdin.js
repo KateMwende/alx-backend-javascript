e88a23a776f8a20ef735a1b7eccf3383af28a2e0
Kate Mwende <Kmwende419@gmail.com>
2023-09-20 12:48:23 +0300

    ended the event on stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,6 +7,6 @@ process.stdin.on('readable', () => {
     process.stdout.write(`Your name is: ${INPUT}`);
   }
 });
-process.on('end', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
