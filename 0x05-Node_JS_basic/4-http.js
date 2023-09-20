649e5fb0ff3d747780c69cd1760bff82de12761e
Kate Mwende <Kmwende419@gmail.com>
2023-09-20 14:12:48 +0300

    Create HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+
+const app = http.createServer((req, res) => {
+  // Set the response headers
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!')
+});
+
+app.listen(PORT, HOST, () => {});
+
+module.exports = app;
