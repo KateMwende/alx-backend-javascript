9974ddec3a583e99e79b266a3f3176c9a1c2c8e3
Kate Mwende <Kmwende419@gmail.com>
2023-09-21 17:00:47 +0300

    changed to decimal value

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const calculateNumber = require("./0-calcul")
 
 describe("calculateNumber", () => {
     it('should return rounded off sum', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 2.6), 4);
         assert.strictEqual(calculateNumber(3.7, 9.5), 13);
     });
