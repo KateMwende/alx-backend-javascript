af4c61a3289b6e8a3a4d1f42df3a3fca3bc562bd
Kate Mwende <Kmwende419@gmail.com>
2023-09-21 17:09:42 +0300

    Edited the expected value

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,7 +4,7 @@ const calculateNumber = require("./0-calcul")
 describe("calculateNumber", () => {
     it('should return rounded off sum', () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 2.6), 4);
-        assert.strictEqual(calculateNumber(3.7, 9.5), 13);
+        assert.strictEqual(calculateNumber(1.5, 2.6), 5);
+        assert.strictEqual(calculateNumber(3.7, 9.5), 14);
     });
 });
