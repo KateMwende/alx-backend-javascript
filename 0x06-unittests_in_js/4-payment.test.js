61c0ef4003d16c5d24b656c852858d13c07d19c2
Kate Mwende <Kmwende419@gmail.com>
2023-09-22 00:24:28 +0300

    Changed stub method

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -10,8 +10,9 @@ describe('sendPaymentRequestToApi', () => {
     
     sendPaymentRequestToApi(100, 20);
 
+    expect(calculateNumberStub.calledOnceWith('SUM', 100, 20)).to.be.true;
     expect(consoleSpy.calledOnceWith('The total is: 10')).to.be.true;
-    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    
     consoleSpy.restore();
     calculateNumberStub.restore();
   });
