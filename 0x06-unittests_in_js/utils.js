9492541b0f8df60aaba39557b6021884d45a323a
Kate Mwende <Kmwende419@gmail.com>
2023-09-23 12:29:54 +0300

    utils file update

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,18 +1,18 @@
 const Utils = {
   calculateNumber(type, a, b) {
-  const roundA = Math.round(a);
-  const roundB = Math.round(b);
+    const roundA = Math.round(a);
+    const roundB = Math.round(b);
     
-  switch (type) {
-    case 'SUM':
-      return roundA + roundB;
-    case 'SUBTRACT':
-      return roundA - roundB;
-    case 'DIVIDE':
-      if (roundB == 0) {
-        return 'Error';  
+    switch (type) {
+      case 'SUM':
+        return roundA + roundB;
+      case 'SUBTRACT':
+        return roundA - roundB;
+      case 'DIVIDE':
+        if (roundB == 0) {
+          return 'Error';  
       }
-      return roundA / roundB;
+        return roundA / roundB;
     default:
       throw new Error('Invalid type');
     }
