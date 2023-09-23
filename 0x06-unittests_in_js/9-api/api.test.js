ae30c383ffce5d298124d9daee6d52c292085e13
Kate Mwende <Kmwende419@gmail.com>
2023-09-23 11:18:54 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('index page testing', () => {
+  const api_url = 'http://localhost:7865';
+  
+  it('GET / returns correct response', (done) => {
+    request.get(`${api_url}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${api_url}/cart/:4`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart :4');
+      done();
+    });
+  });
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${api_url}/cart/:-4`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${api_url}/cart/:hello`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
