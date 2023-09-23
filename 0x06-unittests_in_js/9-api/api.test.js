60c0fd349ca2040bc8c47e21f766bcd1f61058b7
Kate Mwende <Kmwende419@gmail.com>
2023-09-23 11:41:33 +0300

    removed colon

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -13,7 +13,7 @@ describe('index page testing', () => {
   });
 
   it('GET / returns correct response', (done) => {
-    request.get(`${api_url}/cart/:4`, (_err, res, body) => {
+    request.get(`${api_url}/cart/4`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart :4');
       done();
@@ -21,14 +21,14 @@ describe('index page testing', () => {
   });
 
   it('GET / returns 404 response on negatives', (done) => {
-    request.get(`${api_url}/cart/:-4`, (_err, res, _body) => {
+    request.get(`${api_url}/cart/-4`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
 
   it('GET / returns 404 response to non-numerics', (done) => {
-    request.get(`${api_url}/cart/:hello`, (_err, res, _body) => {
+    request.get(`${api_url}/cart/hello`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
